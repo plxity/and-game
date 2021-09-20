2c4fdc764efa091654ad0becd03a330303253e39
Apoorv Taneja <purutaneja.com@gmail.com>
2021-09-21 00:12:00 +0530

    v0.0.1

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = andGame;
+
+function andGame(elements, useSymbol = false) {
+  if (!(elements instanceof Array) || typeof useSymbol !== 'boolean') {
+    return '';
+  }
+
+  const elLength = elements.length;
+  let result = '';
+  elements.forEach((item, i) => {
+    if (i === elLength - 1 && i != 0) {
+      result += `${useSymbol ? ' & ' : 'and '}${item}.`;
+    } else {
+      result += `${item}${i === elLength - 1 ? '.' : i === elLength - 2 ? '' : ', '}`;
+    }
+  });
+  return result;
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+## and-game
+
+```JS
+
+// Input
+andGame(['idli', 'sambhar', 'chutney]);
+
+// Output
+idli, sambhar and chutney.
+
+
+// Input 
+andGame(['idli', 'sambhar', 'chutney], true);
+
+// Output
+idli, sambhar & chutney.
+
+```
+
